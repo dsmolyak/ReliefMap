b02cd61115adbb0816c3e5fb972c202b85a68efb
robby <robbyef98@gmail.com>
2016-11-05 16:21:23 -0400

    hypothetically will use html five in the future

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,36 @@
-function initMap() {
-    var uluru = {lat: -25.363, lng: 131.044};
-    var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 4,
-        center: uluru
-    });
-    var marker = new google.maps.Marker({
-        position: uluru,
-        map: map
-    });
-}
+var me = {};
+    function initMap() {
+        var x = document.getElementById("demo");
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function(position) {
+                setMe(position);
+            });
+        } else {
+            var geocoder = new google.maps.Geocoder();
+            if(google.loader.ClientLocation) {
+                me.lat = google.loader.ClientLocation.latitude;
+                me.lng = google.loader.ClientLocation.longitude;
+            }
+        }
+        x.innerHTML = "Latitude: " + me.lat +
+            "<br>Longitude: " + me.lng;
+        dispMap(me);
+    }
+
+    function setMe (position) {
+        me.lat = position.coords.latitude;
+        me.lng = position.coords.longitude;
+
+    }
+
+
+    function dispMap(cur) {
+        var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 12,
+            center: cur
+        });
+        var marker = new google.maps.Marker({
+            position: cur,
+            map: map
+        });
+    }
